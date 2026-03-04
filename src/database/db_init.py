692ddce3b3b7109fb18e6167c95bb4b665d4d2b7
Caua Carvalho <cauaccarvalho10@gmail.com>
2026-03-04 16:01:35 -0300

    refactor: remove unused language model import from db_init

--- src/database/db_init.py
+++ src/database/db_init.py
@@ -2,7 +2,6 @@ from src.database.base import Base
 from src.database.session import engine
 from src.models.analysis_model import Analysis
 from src.models.job_model import Job
-from src.models.language_model import Language
 from src.models.resume_model import Resume
 from src.models.education_levels_model import EducationLevel
 
