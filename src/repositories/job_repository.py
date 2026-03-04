595e7002fe3fe07372e06bc7da0e26fcb9a625ae
Caua Carvalho <cauaccarvalho10@gmail.com>
2026-03-04 10:16:43 -0300

    implement repository layer for database access

---
+++ src/repositories/job_repository.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from typing import List
+from src.models.job_model import Job
+from datetime import datetime
+
+
+class JobRepository:
+
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_all(self) -> List[Job]:
+        query  = select(Job).order_by(Job.created_at.desc())
+        result = self.db.execute(query )
+        return result.scalars().all()
+    
+    def create(self, name: str, main_activities: str, prerequisites: str, differentials: str) -> Job:
+        job = Job(
+            name=name,
+            main_activities=main_activities,
+            prerequisites=prerequisites,
+            differentials=differentials,
+            created_at=datetime.utcnow()
+        )
+
+        self.db.add(job)
+        self.db.commit()
+        self.db.refresh(job)
+
+        return job
